c27900215d010e535fd8aad94bc0cf6be123bd4d
jannchie <jannchie@gmail.com>
2020-10-06 16:25:09 +0900

    Fix: Use FFmpeg

--- src/ffmpeg.js
+++ src/ffmpeg.js
@@ -7,13 +7,13 @@ const ffmpeg = createFFmpeg({
   process: p => console.log(p)
 });
 
-async function pngToMp4(pngPath, name, outPathAndName, thread = 8) {
+async function pngToMp4(pngPath, name, outPathAndName, fps, thread = 8) {
   await ffmpeg.load();
   let nameList = fs.readdirSync(pngPath)
   for (let name of nameList) {
     await ffmpeg.write(name, `${pngPath}${name}`);
   }
-  await ffmpeg.run(`-r 60 -threads ${thread} -i ${name}-%d.png out.mp4`)
+  await ffmpeg.run(`-r ${fps} -threads ${thread} -i ${name}-%d.png out.mp4`)
   const data = await ffmpeg.read('out.mp4');
   fs.writeFileSync(outPathAndName, data)
   process.exit(0);
