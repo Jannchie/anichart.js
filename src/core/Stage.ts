5ffc172f9bd427f7118648088c5c6ba4a8fb9323
jannchie <jannchie@gmail.com>
2021-01-15 03:50:37 +0900

    feat(output): split

--- src/core/Stage.ts
+++ src/core/Stage.ts
@@ -2,7 +2,7 @@ import * as d3 from "d3";
 import { Ani } from "./ani/Ani";
 import { canvasRenderer, CanvasRenderer } from "./CanvasRenderer";
 import { Component } from "./component/Component";
-import { addFrameToFFmpeg, ffmpeg, outputMP4 } from "./FFmpeg";
+import { addFrameToFFmpeg, ffmpeg, outputMP4, removePNG } from "./FFmpeg";
 import { recourse } from "./Recourse";
 import * as async from "async";
 export class Stage {
@@ -11,6 +11,10 @@ export class Stage {
   renderer: CanvasRenderer;
 
   options = { sec: 5, fps: 30 };
+  outputOptions = {
+    fileName: "output",
+    splitSec: 60,
+  };
   interval: d3.Timer;
   output: boolean;
   outputConcurrency = 128;
@@ -41,8 +45,8 @@ export class Stage {
   constructor(canvas?: HTMLCanvasElement) {
     if (!canvas) {
       canvas = document.createElement("canvas");
-      canvas.width = 800;
-      canvas.height = 600;
+      canvas.width = 1920;
+      canvas.height = 1080;
       document.body.appendChild(canvas);
     }
     this.renderer = canvasRenderer;
@@ -73,19 +77,42 @@ export class Stage {
       }
       if (this.output) {
         ffmpeg.load().then(() => {
-          const frames: number[] = [];
-          while (this.cFrame < this.totalFrames) {
-            this.cFrame++;
-            frames.push(this.cFrame);
+          const partCount =
+            Math.floor(this.options.sec / this.outputOptions.splitSec) + 1;
+          let part = 0;
+          const parts: number[] = [];
+          while (part++ < partCount) {
+            parts.push(part);
           }
           async
-            .eachLimit(frames, this.outputConcurrency, (f, callback) => {
-              this.render(f / this.options.fps);
-              addFrameToFFmpeg(this.canvas, f).then(() => callback());
+            .eachSeries(parts, (p, callback) => {
+              const frames: number[] = [];
+              const picNameList: string[] = [];
+              while (
+                this.cFrame < this.totalFrames &&
+                this.cFrame < p * this.outputOptions.splitSec * this.options.fps
+              ) {
+                this.cFrame++;
+                frames.push(this.cFrame);
+              }
+              async
+                .eachLimit(frames, this.outputConcurrency, (f, cb) => {
+                  this.render(f / this.options.fps);
+                  const no =
+                    f -
+                    (p - 1) * this.outputOptions.splitSec * this.options.fps;
+                  picNameList.push(`output-${no}.png`);
+                  addFrameToFFmpeg(this.canvas, no).then(() => cb());
+                })
+                .then(() => {
+                  outputMP4(this.options.fps).then(() => {
+                    removePNG(picNameList);
+                    callback();
+                  });
+                });
             })
-            .then(() => {
-              outputMP4(this.options.fps);
-            });
+            // tslint:disable-next-line:no-console
+            .then(() => console.log("finished!"));
         });
       } else {
         this.interval = d3.interval((elapsed) => {
