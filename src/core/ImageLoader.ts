9ce99247c0767044fce4c5542e5ddf32521abf19
jannchie <jannchie@gmail.com>
2021-01-14 16:21:31 +0900

    remove: node support, useless dependencies

--- src/core/ImageLoader.ts
+++ src/core/ImageLoader.ts
@@ -2,22 +2,14 @@ type LoadImageFunc = (url: string) => Promise<CanvasImageSource>;
 export class ImageLoader {
   load: LoadImageFunc;
   constructor() {
-    if (typeof window !== "undefined") {
-      // Browser
-      const { Image } = require("@canvas/image");
-      this.load = (url: string) =>
-        new Promise((resolve) => {
-          const image = new Image();
-          image.onload = () => {
-            resolve(image);
-          };
-          image.src = url;
-        });
-    } else {
-      // Node.js
-      const { loadImage } = require("canvas");
-      this.load = loadImage;
-    }
+    this.load = (url: string) =>
+      new Promise((resolve) => {
+        const image = new Image();
+        image.onload = () => {
+          resolve(image);
+        };
+        image.src = url;
+      });
   }
 }
 
