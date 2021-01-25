aa50cc173da9ddabd9d58beef27e51247950345f
jannchie <jannchie@gmail.com>
2021-01-25 19:08:12 +0900

    feat: add built-in advance component

---
+++ src/core/custom/TextWithBackGround.ts
@@ -0,0 +1,50 @@
+import {
+  FontWeight,
+  TextAlign,
+  TextBaseline,
+  Component,
+  canvasHelper,
+  Rect,
+  Text,
+} from "../..";
+
+export function getTextWithBackground({
+  txt = "在此处输入文字！",
+  position = { x: 100, y: 100 },
+  fontSize = 24,
+  fontWeight = "bold" as FontWeight,
+  textAlign = "right" as TextAlign,
+  textBaseline = "top" as TextBaseline,
+  foregroundStyle = "#1e1e1e",
+  backgroundStyle = "#fff",
+  padding = { top: 5, bottom: 0, left: 5, right: 5 },
+}) {
+  const res = new Component({
+    position,
+  });
+  const text = new Text({
+    text: txt,
+    position: { x: padding.left, y: padding.top },
+    fillStyle: foregroundStyle,
+    fontSize,
+    textBaseline,
+    fontWeight,
+    textAlign,
+  });
+  const width = canvasHelper.measure(text).width;
+  const rect = new Rect({
+    clip: true,
+    shape: {
+      width: width + text.position.x + padding.left + padding.right,
+      height: fontSize + padding.top + padding.bottom,
+    },
+    fillStyle: backgroundStyle,
+  });
+  if (textAlign === "right") {
+    text.position.x += width;
+  }
+  res.center = { x: rect.shape.width / 2, y: rect.shape.height / 2 };
+  rect.children.push(text);
+  res.children.push(rect);
+  return res;
+}
