f96c942c32f1918fb2227020eafd56d9bcdc3df7
jannchie <jannchie@gmail.com>
2021-01-25 21:15:34 +0900

    feat: wrapper

---
+++ src/core/wrapper/Fade.ts
@@ -0,0 +1,27 @@
+import { Ani, Component } from "../..";
+
+export function addFadeWrapper(
+  child: Component | Ani,
+  alpha: (sec: number) => number
+) {
+  const ani = new Ani();
+  const comp = new Component();
+  if (child instanceof Component) {
+    comp.children.push(child);
+    ani.getComponent = (sec) => {
+      const a = alpha(sec);
+      comp.alpha = a;
+      return comp;
+    };
+  } else {
+    ani.getComponent = (sec) => {
+      const c = child.getComponent(sec);
+      if (c == null) return null;
+      const a = alpha(sec);
+      c.alpha = a;
+      comp.children = [c];
+      return comp;
+    };
+  }
+  return ani;
+}
--- src/core/wrapper/Scale.ts
+++ src/core/wrapper/Scale.ts
@@ -1,31 +1,44 @@
 import { Ani } from "../ani/Ani";
 import { Component } from "../component/Component";
-import { Rect } from "../component/Rect";
-
 export function addScaleWrapper(
   child: Ani | Component,
-  scale: (sec: number) => { x: number; y: number }
+  scale:
+    | ((sec: number) => { x: number; y: number })
+    | { x: number; y: number }
+    | ((sec: number) => number)
 ) {
+  let scaleFunc: (sec: number) => { x: number; y: number } | number;
+  if (typeof scale === "function") {
+    scaleFunc = scale;
+  } else {
+    scaleFunc = () => scale;
+  }
   const ani = new Ani();
   const wrapper = new Component();
   if (child instanceof Ani) {
     ani.getComponent = (sec) => {
       const comp = child.getComponent(sec);
       if (!comp) return null;
-      wrapper.position = { x: comp.position.x, y: comp.position.y };
-      comp.position = { x: 0, y: 0 };
-      wrapper.scale = scale(sec);
-      wrapper.children = [comp];
-      return wrapper;
+      return getComp(sec, comp);
     };
   } else {
     wrapper.children.push(child);
     wrapper.position = { x: child.position.x, y: child.position.y };
     ani.getComponent = (sec) => {
-      wrapper.scale = scale(sec);
-      child.position = { x: 0, y: 0 };
-      return wrapper;
+      return getComp(sec, child);
     };
   }
   return ani;
+
+  function getComp(sec: number, comp: Component) {
+    let s = scaleFunc(sec);
+    if (typeof s === "number") {
+      s = { x: s, y: s };
+    }
+    wrapper.position = { x: comp.position.x, y: comp.position.y };
+    comp.position = { x: 0, y: 0 };
+    wrapper.scale = s;
+    wrapper.children = [comp];
+    return wrapper;
+  }
 }
